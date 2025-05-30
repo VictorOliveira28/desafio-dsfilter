a9ed504bf6e63f54b7f511abfaeaca47cdbce6d4
Victor Oliveira <victor.oliveira20@icloud.com>
2025-05-30 18:27:49 -0300

    Layout completed

---
+++ src/components/ListingBody/Listing/index.tsx
@@ -0,0 +1,34 @@
+import "./styles.css";
+
+export default function Listing() {
+  return (
+    <section className="dsf-section-container">
+      <div className="dsf-card-product-list">
+        <div className="dsf-product">
+          <p className="dsf-product-name">PC Gamer Pro</p>
+          <p className="dsf-product-price">R$ 1200.00</p>
+        </div>
+        <div className="dsf-product">
+          <p className="dsf-product-name">PC Gamer Pro</p>
+          <p className="dsf-product-price">R$ 1200.00</p>
+        </div>
+        <div className="dsf-product">
+          <p className="dsf-product-name">PC Gamer Pro</p>
+          <p className="dsf-product-price">R$ 1200.00</p>
+        </div>
+        <div className="dsf-product">
+          <p className="dsf-product-name">PC Gamer Pro</p>
+          <p className="dsf-product-price">R$ 1200.00</p>
+        </div>
+        <div className="dsf-product">
+          <p className="dsf-product-name">PC Gamer Pro</p>
+          <p className="dsf-product-price">R$ 1200.00</p>
+        </div>
+        <div className="dsf-product">
+          <p className="dsf-product-name">PC Gamer Pro</p>
+          <p className="dsf-product-price">R$ 1200.00</p>
+        </div>
+      </div>
+    </section>
+  );
+}
